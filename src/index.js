93dff6cbaa4c456f24739abec5d22c99414464a9
Sofya Tarnalitskaya <tarnalitska@gmail.com>
2021-03-04 16:34:51 +0300

    feat: brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+     if (str.length % 2 != 0){
+		return false;
+	}
+ 	let arr = str.split('');
+ 	let stack = [];
+ 	for ( let i = 0; i < arr.length; i++){
+ 		for ( let j = 0; j < bracketsConfig.length; j++ ){
+ 			if (arr[i] == bracketsConfig[j][1] && stack[stack.length-1] == bracketsConfig[j][0]){
+ 				stack.pop();
+ 			} else if (arr[i] == bracketsConfig[j][0]){
+ 				stack.push(arr[i]);
+ 			}
+ 		}
+ 	}
+
+ 	if (stack.length == 0){
+ 		return true;
+ 	}
+ 	return false;
 }
